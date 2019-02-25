a3d95acb3f62b4288206fd57c0fef5bbca2a81ce
jnshu2040 <liuqiyong0319@live.com>
2019-02-25 18:54:22 +0800

    add  routes

--- trunk/src/app/app-routing.module.ts
+++ trunk/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "reLogin",//path不能以斜杠'/'开头
+    component: null //reloginComponent
+  },
+  {
+    path: "home",
+    component: null //home
+  },
+  {
+    path: "help",
+    component: null //helpPageComponent
+  },
+  {
+    path: "upload",
+    component: null //uploadMyMusic
+  },
+  {
+    path: '',
+    redirectTo: 'reLogin',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: null//PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+
